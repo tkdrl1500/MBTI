0b3275dc85835af5d58c1cd0d55bd2aa5a406a22
tkdrl1500 <70390705+tkdrl1500@users.noreply.github.com>
2021-06-05 00:15:21 +0900

    JavaScript: 화면 전환 및 질문

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const dropDownBtn = document.querySelector(".navBar-btn");
 const dropDownBox = document.querySelector(".dropDownBox");
 const startBtn = document.querySelector(".startBtn");
 const inputBar = document.querySelector("#inputBar-text");
+const qna = document.querySelector(".qna");
+const answerFist = document.querySelector(".answerF");
+const answerSecond = document.querySelector(".answerS");
+const contentFist = document.querySelector("#contentFist");
+const contentSecond = document.querySelector("#contentSecond");
 
 dropDownBtn.addEventListener("click", () => {
   dropDownBox.classList.toggle("show");
@@ -16,5 +21,30 @@ dropDownBtn.addEventListener("blur", () => {
 startBtn.addEventListener("click", () => {
   if (inputBar.value === "") {
     alert("내용을 입력하세요!");
+  } else {
+    contentFist.style.WebkitAnimation = "fadeOut 1s";
+    contentFist.style.animation = "fadeOut 1s";
+    setTimeout(() => {
+      contentSecond.style.WebkitAnimation = "fadeIn 1s";
+      contentSecond.style.animation = "fadeIn 1s";
+      setTimeout(() => {
+        contentFist.style.display = "none";
+        contentSecond.style.display = "block";
+      }, 400);
+    }, 400);
   }
 });
+
+// 질문 창
+function goNext() {
+  let q = qna;
+  q.innerHTML = questionList[0].q;
+
+  let a = answerFist;
+  a.innerHTML = questionList[0].a[0].answer;
+
+  let b = answerSecond;
+  b.innerHTML = questionList[0].a[1].answer;
+}
+
+goNext();
